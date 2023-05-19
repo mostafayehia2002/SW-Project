6f67b4b0995092685095bc361f7d04c96268c63f
Mostafa1682002 <ma9856603@gmail.com>
2023-05-19 15:50:43 +0300

    add table subject doctor

--- Admins/Department_Setting.php
+++ Admins/Department_Setting.php
@@ -11,10 +11,7 @@ if (isset($_GET['department_id'])) {
     $department_table = $department_table->fetch(PDO::FETCH_ASSOC);
     $department_name = $department_table['Department_English_Name'];
     $table_subject = $department_name . "_subject";
-    $department_doctor_subject= $department_name."_doctor_subjects";
-
-  
- 
+    $department_doctor_subject = $department_name . "_doctor_subjects";
 } else {
     header("location: Department_Info.php");
     exit();
@@ -28,7 +25,7 @@ if (isset($_POST['add_subject'])) {
     $subject_name = $_POST['subject_name'];
     $subject_hour = $_POST['subject_hours'];
     $de_subject_name = $_POST['de_subject_name'];
-  
+
 
 
     $subject_exist = $con->query("SELECT * FROM `$table_subject` WHERE `Subject_Name`='$subject_name' ");
@@ -39,7 +36,7 @@ if (isset($_POST['add_subject'])) {
             echo "<div class='success'>  تم اضافه الماده بنجاح</div>";
         }
     } else {
-            echo "<div class='faild'>هذه الماده موجوده مسبقا </div>";
+        echo "<div class='faild'>هذه الماده موجوده مسبقا </div>";
     }
 }
 //ubdate department subject
@@ -49,15 +46,24 @@ if (isset($_POST['add_subject'])) {
 
 //delete department subject
 
-if(isset($_GET['delete_subject'])){
-
-$delete_subject=$_GET['delete_subject'];
-
- $delete=$con->query("DELETE FROM `$table_subject`  WHERE `ID`='$delete_subject'");
-if($delete){
-    header("location:Department_Setting.php?department_id=$id&status=add_subject");
-    exit;
+if (isset($_GET['delete_subject'])) {
+    $delete_subject = $_GET['delete_subject'];
+    $delete = $con->query("DELETE FROM `$table_subject`  WHERE `ID`='$delete_subject'");
+    if ($delete) {
+        header("location:Department_Setting.php?department_id=$id&status=add_subject");
+        exit;
+    }
 }
+
+//delete department subject
+
+if (isset($_GET['delete_drsubject'])) {
+    $delete_drsubject = $_GET['delete_drsubject'];
+    $delete = $con->query("DELETE FROM `$department_doctor_subject`  WHERE `id`='$delete_drsubject'");
+    if ($delete) {
+        header("location:Department_Setting.php?department_id=$id&status=add_doctor_subject");
+        exit;
+    }
 }
 
 
@@ -88,8 +94,8 @@ if (isset($_POST['add_doctor_subject'])) {
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-           <!-- table -->
-     <link rel="stylesheet" href="../CssComponent/bootstrap.min.css">
+    <!-- table -->
+    <link rel="stylesheet" href="../CssComponent/bootstrap.min.css">
     <link rel="stylesheet" href="../CssComponent/datatables.min.css">
     <!--  -->
     <link rel="stylesheet" href="../CssComponent/all.min.css">
@@ -153,7 +159,7 @@ if (isset($_POST['add_doctor_subject'])) {
                                 <label for="Subject_Hours">
                                     عدد ساعات الماده
                                 </label>
-                                <input type="number" id="Subject_Hours" name="subject_hours" required  value="3"/>
+                                <input type="number" id="Subject_Hours" name="subject_hours" required value="3" />
                             </div>
 
 
@@ -161,11 +167,11 @@ if (isset($_POST['add_doctor_subject'])) {
                                 <label for="De_Subject_Name"> اسم المتطلب السابق</label>
                                 <input type="text" id="De_Subject_Name" name="de_subject_name" required>
                             </div>
-                         
-                        
-                          
 
-            
+
+
+
+
 
                         </div>
                         <div class="save">
@@ -174,73 +180,56 @@ if (isset($_POST['add_doctor_subject'])) {
                     </div>
                 </form>
                 <!-- show table data -->
-                     
-            <div class="row">
-                <h3 class="titleTabel">جدول لعرض بيانات مواد القسم </h3>
-            <div class="col-12">
-                <div class="data_table">
-                    <table id="example" class="table table-striped table-bordered">
-                        <thead class="table-dark">
-                            <tr>
-                            <th class="th_text">ID</th>
-                            <th class="th_text">كود الماده</th>
-                            <th class="th_text">الاسم الماده</th>
-                            <th class="th_text">عدد ساعات </th>
-                            <th class="th_text">اسم المتطلب السابق</th>
-                            <th class="th_text">التحكم</th>
-                            </tr>
-                        </thead>
-                        
-                        <tbody class="table-content">
+                <div class="row">
+                    <h3 class="titleTabel">جدول لعرض بيانات مواد القسم </h3>
+                    <div class="col-12">
+                        <div class="data_table">
+                            <table id="example" class="table table-striped table-bordered">
+                                <thead class="table-dark">
+                                    <tr>
+                                        <th class="th_text">ID</th>
+                                        <th class="th_text">كود الماده</th>
+                                        <th class="th_text">الاسم الماده</th>
+                                        <th class="th_text">عدد ساعات </th>
+                                        <th class="th_text">اسم المتطلب السابق</th>
+                                        <th class="th_text">التحكم</th>
+                                    </tr>
+                                </thead>
+                                <tbody class="table-content">
+                                    <?php
+                                    $all_doctor_subject = $con->query("SELECT * FROM `$table_subject`");
+                                    $doctor_subject_data = $all_doctor_subject->fetchAll(PDO::FETCH_ASSOC);
+                                    if (!empty($doctor_subject_data)) {
+                                        foreach ($doctor_subject_data as  $dr) {
+                                    ?>
+                                            <tr>
+                                                <td><?php echo $dr['ID'] ?></td>
+                                                <td><?php echo $dr['Subject_Code'] ?></td>
+                                                <td><?php echo $dr['Subject_Name'] ?></td>
+                                                <td><?php echo $dr['Subject_Hours'] ?></td>
+                                                <td><?php echo $dr['De_Subject_Name'] ?></td>
+                                                <td>
+                                                    <button><a class="update" href=" " ?update=<?php echo $dr['ID'] ?> class="delete-btn"><i class="fa-solid fa-edit"></i> تعديل</a>
+                                                    </button>
+                                                    <button><a class="delete" href="Department_Setting.php? department_id=<?php echo $id ?>&status=add_subject&delete_subject=<?php echo $dr['ID'] ?>" class="delete-btn" onclick="return confirm('Are you soure Delete this subject ?');"><i class="fa-solid fa-trash"></i> حذف</a> </button>
+                                                </td>
+                                            </tr>
+                                    <?php
+                                        }
+                                    } else {
+                                        echo '<p class="empty">no doctor available</p>';
+                                    }
+                                    ?>
 
-                            <?php
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
 
-                        $all_doctor_subject = $con->query("SELECT * FROM `$table_subject`");
 
-                        $doctor_subject_data= $all_doctor_subject->fetchAll(PDO::FETCH_ASSOC);
 
-                        
-
-                        if (!empty($doctor_subject_data)) {
-                            foreach ($doctor_subject_data as  $dr) {
-                        ?>
-                            <tr>
-                                <td><?php echo $dr['ID'] ?></td>
-                                <td><?php echo $dr['Subject_Code'] ?></td>
-                                <td><?php echo $dr['Subject_Name'] ?></td>
-                                <td><?php echo $dr['Subject_Hours'] ?></td>
-                                <td><?php echo $dr['De_Subject_Name'] ?></td>
-                                  
-                                <td>
-                                <button><a  class="update" href=" "?update=<?php echo $dr['ID'] ?>
-                                        class="delete-btn"><i class="fa-solid fa-edit"></i> تعديل</a> </button>
-
-                                 <button ><a class="delete"
-                                 
-                                 href="Department_Setting.php? department_id=<?php echo $id ?>&status=add_subject&delete_subject=<?php echo $dr['ID'] ?>" class="delete-btn"
-                                        onclick="return confirm('Are you soure Delete this subject ?');"><i
-                                            class="fa-solid fa-trash"></i> حذف</a> </button>
-
-                                    
-                                            
-                                </td>
-                            </tr>
-                            <?php
-                            }
-                        } else {
-                            echo '<p class="empty">no doctor available</p>';
-                        }
-                        ?>                      
-                       
-                        </tbody>
-                    </table>
                 </div>
-            </div>
 
-
-                   
-
-           
             <?php } elseif (isset($_GET['status']) && $_GET['status'] == "add_doctor_subject") {  ?>
 
                 <form action="" method="POST" enctype="multipart/form-data">
@@ -254,31 +243,23 @@ if (isset($_POST['add_doctor_subject'])) {
                     <div class="break"></div>
 
                     <div class="oneuser">
-                        <h3 class="title">اضافه المواد لي الدكتور</h3>
-
+                        <h3 class="title">اضافه الموادللدكتور</h3>
                         <div class="allinput">
                             <div class="input-filed">
                                 <label for="list1">الماده</label>
                                 <?php
                                 $subject = $con->query("SELECT * FROM `$table_subject` ");
                                 $all_subjects = $subject->fetchAll(PDO::FETCH_ASSOC);
-
-        
                                 ?>
                                 <input list="list1" name="subject" required />
                                 <datalist id="list1">
                                     <?php
                                     foreach ($all_subjects as $s1) { ?>
-
-                                        <option value="<?php echo $s1['Subject_Code'] ?>"><?php echo $s1['Subject_Name'] ?>
+                                        <option value="<?php echo $s1['Subject_Code'] ?>">
+                                            <?php echo $s1['Subject_Name'] ?>
                                         </option>
-
-
-                                 <?php }  ?>
-
+                                    <?php }  ?>
                             </div>
-
-
                             <div class="input-filed">
                                 <label for="list2">الدكتور</label>
                                 <?php
@@ -289,7 +270,8 @@ if (isset($_POST['add_doctor_subject'])) {
                                 <datalist id="list2">
                                     <?php
                                     foreach ($data_doctor as $d) { ?>
-                                        <option value="<?php echo $d['Doctor_ID'] ?>"><?php echo $d['Full_Name'] ?></option>
+                                        <option value="<?php echo $d['Doctor_ID'] ?>"><?php echo $d['Full_Name'] ?>
+                                        </option>
                                     <?php  } ?>
 
                                 </datalist>
@@ -301,8 +283,52 @@ if (isset($_POST['add_doctor_subject'])) {
                     </div>
                 </form>
 
-                <!-- show table of subject -->
+                <!-- show table Doctors Subject  -->
+                <div class="row">
+                    <h3 class="titleTabel">جدول لعرض بيانات دكاترة المواد </h3>
+                    <div class="col-12">
+                        <div class="data_table">
+                            <table id="example" class="table table-striped table-bordered">
+                                <thead class="table-dark">
+                                    <tr>
+                                        <th class="th_text">ID</th>
+                                        <th class="th_text">اسم الدكتور الماده</th>
+                                        <th class="th_text">اسم الماده</th>
+                                        <th class="th_text">التحكم</th>
+                                    </tr>
+                                </thead>
+                                <tbody class="table-content">
+                                    <?php
+                                    $doctor_subject = $con->query("SELECT id,Subject_Name,Full_Name FROM `$department_doctor_subject` ds INNER JOIN `doctor` dc on ds.Doctor_Id= dc.Doctor_ID");
+                                    $doctor_subject = $doctor_subject->fetchAll(PDO::FETCH_ASSOC);
 
+                                    if (!empty($doctor_subject)) {
+                                        foreach ($doctor_subject as  $dr_sub) {
+                                    ?>
+                                            <tr>
+                                                <td><?php echo $dr_sub['id'] ?></td>
+                                                <td><?php echo $dr_sub['Subject_Name'] ?></td>
+                                                <td><?php echo $dr_sub['Full_Name'] ?></td>
+                                                <td>
+                                                    <button><a class="update" href="" ?update=<?php echo $dr_sub['id'] ?> class="delete-btn"><i class="fa-solid fa-edit"></i> تعديل</a>
+                                                    </button>
+                                                    <button><a class="delete" href="Department_Setting.php? department_id=<?php echo $id ?>&status=add_subject&delete_drsubject=<?php echo $dr_sub['id'] ?>" class="delete-btn" onclick="return confirm('Are you soure Delete this subject ?');"><i class="fa-solid fa-trash"></i> حذف</a> </button>
+                                                </td>
+                                            </tr>
+                                    <?php
+                                        }
+                                    } else {
+                                        echo '<p class="empty">no doctor available</p>';
+                                    }
+                                    ?>
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+
+
+
+                </div>
 
 
 
