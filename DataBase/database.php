fef4b1f719d8cf12d2cb4a17b78bc13fb0108787
teefa <gad993813@gmail.com>
2023-05-26 16:08:40 +0300

    finished

---
+++ DataBase/database.php
@@ -0,0 +1,13 @@
+<?php
+try {
+    $host = 'localhost';
+    $dbname = 'sw-project';
+    $user = 'root';
+    $password = '';
+    //$user = 'teefa';
+    //$password ='2002';
+
+    $con = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $user, $password);
+} catch (PDOException $e) {
+    echo  $e->getMessage();
+}
